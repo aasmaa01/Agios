6b658144a3b5819762faa53a417c9ef261c01696
aasmaa01 <aasmaelearn@gmail.com>
2026-02-05 02:19:12 +0100

    feat: add user auth routes (signup, login, logout)

---
+++ backend/routes/user.routes.js
@@ -0,0 +1,10 @@
+import express from 'express'
+import { userLogin, userSignup, userLogout} from '../controllers/account.controller.js';
+
+const userRouter= express.Router();
+
+userRouter.post('/Login', userLogin)
+userRouter.post('/Signup', userSignup)
+userRouter.post('/logout', logoutUser)
+
+export default userRouter;
